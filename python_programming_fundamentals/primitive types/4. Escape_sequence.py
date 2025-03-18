e6aa2e4d7b4ca311efb06aa04d6e3e80f0cf48bc
ursarahim <abdurcse13@gmail.com>
2025-03-18 16:06:05 +0600

    Add escape sequence doc

---
+++ python_programming_fundamentals/primitive types/4. Escape_sequence.py
@@ -0,0 +1,12 @@
+course_name = "Python \"Programming\" Fundamentals"
+print(course_name)
+
+course_name = 'Python "Programming" Fundamentals'
+print(course_name)
+
+course_name = "Python \nProgramming \nFundamentals"
+print(course_name)
+
+course_name = "Python \tProgramming \tFundamentals"
+print(course_name)
+
