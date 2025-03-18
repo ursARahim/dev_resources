45b8b8d789d22a9ce076f1a05dab3524d6885c8e
ursarahim <abdurcse13@gmail.com>
2025-03-18 15:49:17 +0600

    Add string doc

---
+++ python_programming_fundamentals/primitive types/3. Strings.py
@@ -0,0 +1,21 @@
+course_name = "Python Programming Fundamentals"
+print(course_name)
+
+message = """
+Hello,
+
+This is a message from the course.
+
+Thanks for joining the course.
+
+"""
+print(message)
+
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+
+
