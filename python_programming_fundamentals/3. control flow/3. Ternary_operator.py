c656cde9d411ad8e0d165bfcad8cd5b66974d69f
ursarahim <abdurcse13@gmail.com>
2025-03-23 15:55:21 +0600

    Add ternary operator doc

---
+++ python_programming_fundamentals/3. control flow/3. Ternary_operator.py
@@ -0,0 +1,12 @@
+age = 22
+if age >= 18:
+    message = "Elgible"
+else:
+    message = "Not eligible"
+
+print(message)
+
+
+age = 22
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
