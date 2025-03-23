c9e418e50383beee9a22f004c3abf4d0c930e708
ursarahim <abdurcse13@gmail.com>
2025-03-23 15:03:22 +0600

    Add conditional logic

---
+++ python_programming_fundamentals/3. control flow/2. Conditional_statements.py
@@ -0,0 +1,26 @@
+temperature = 35
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+print("Done")
+
+
+
+temperature = 25
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+print("Done")
+
+
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
